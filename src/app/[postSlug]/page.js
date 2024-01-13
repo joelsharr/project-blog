ccf7c414bd6cc854292fd5fbf87725bacb60adfc
Joel Harrison <Joel.Harrison@netscout.com>
2024-01-13 16:29:35 -0700

    Exercise 5a: Embedded Components

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,7 +6,7 @@ import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { BLOG_TITLE } from '@/constants';
-import CodeSnippet from '@/components/CodeSnippet';
+import COMPONENT_MAP from '@/helpers/mdx-components';
 
 export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
@@ -27,9 +27,7 @@ async function BlogPost({ params }) {
       <div className={styles.page}>
         <MDXRemote
           source={content}
-          components={{
-            pre: CodeSnippet
-          }}
+          components={COMPONENT_MAP}
         />
       </div>
     </article>
